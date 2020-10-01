70b683279f5fd39fba85e0fd2a1baaa9805d9fe6
LexTheMenace <lexthemenace@gmail.com>
2020-10-01 15:46:46 -0400

    react setup

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
+
+import { Container } from 'reactstrap'
+
+function App() {
+  return (
+    <div className="App">
+      <Container>
+        <h1>Job App</h1>
+      </Container>
+      
+    </div>
+  );
+}
+
+export default App;
