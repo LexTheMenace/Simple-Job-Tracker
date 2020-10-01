e355b50a83b3ef7a1dc4b8631193e30b6479daf1
LexTheMenace <lexthemenace@gmail.com>
2020-10-01 15:29:32 -0400

    create and delete routes

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -7,5 +7,18 @@ module.exports = {
         .sort({ date: -1 })
         .then(jobs => res.json(jobs))
         .catch(err => res.status(422).json(err));
+    },
+    create: (req, res) => {
+      db.Job
+        .create(req.body)
+        .then(job => res.json(job))
+        .catch(err => res.status(422).json(err));
+    },
+    remove: (req, res) => {
+        db.Job
+          .findById({ _id: req.params.id })
+          .then(job => job.remove())
+          .then(job => res.json({ success: true, deletedData: job }))
+          .catch(err => res.status(404).json({ success: false, msg: err }));
     }
   };
--- routes/api/jobs.js
+++ routes/api/jobs.js
@@ -4,5 +4,10 @@ const jobsController = require("../../controllers/jobsController");
 // api/jobs
 router.route("/")
   .get(jobsController.find)
+  .post(jobsController.create);
+
+router
+  .route("/:id")
+  .delete(jobsController.remove);
 
 module.exports = router;
